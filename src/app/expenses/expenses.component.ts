3b40470dd70f43fe98366fcbdd4b55eae8318e84
Paloma <paloma.gs23@gmail.com>
2018-09-07 14:59:54 -0300

    Adicionado flexLayout, Criado options para a escolha de filtragem dos objetos

--- src/app/expenses/expenses.component.ts
+++ src/app/expenses/expenses.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'challenge-expenses',
-    templateUrl: 'expenses.component.html'
+    templateUrl: 'expenses.component.html',
+    styleUrls: ['expenses.component.css']
 })
 
 export class ExpensesComponent implements OnInit {
--- src/app/expenses/expenses.module.ts
+++ src/app/expenses/expenses.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ExpensesComponent } from './expenses.component';
+import { MatRadioModule, MatToolbarModule } from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
-    imports: [],
+    imports: [
+        FlexLayoutModule,
+        MatRadioModule,
+        MatToolbarModule
+    ],
     exports: [ExpensesComponent],
     declarations: [ExpensesComponent],
     providers: []
