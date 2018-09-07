4a3196378ba120768921f2fc541038a7491b394d
Paloma <paloma.gs23@gmail.com>
2018-09-06 22:36:31 -0300

    Adicionado angular material ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavModule } from './nav/nav.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
